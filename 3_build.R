8bc61bc591a6b24257c5e3abf5dd7e2da7aaa0f5
Platt <lplatt@usgs.gov>
2021-11-24 08:14:53 -0500

    fix classes

--- 3_build.R
+++ 3_build.R
@@ -18,10 +18,10 @@ p3_targets <- list(
     add_poly_group_to_svg(
       out_svg = "3_build/tmp/state_paths.svg",
       in_svg = root_svg,
-      grp_id = "conus-states",
+      grp_id = "conus_states",
       paths = p2_conus_states_paths,
       path_ids = p2_conus_states_names,
-      path_class = "state"),
+      path_class = rep("state", length(p2_conus_states_paths))),
     format = "file"
   ),
   
@@ -34,7 +34,7 @@ p3_targets <- list(
       grp_id = 'hucs',
       paths = p2_huc8s_paths,
       path_ids = p0_huc8_grps,
-      path_class = "huc8"),
+      path_class = rep("huc8", length(p2_huc8s_paths))),
     format = "file"
   ),
   
